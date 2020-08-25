39c46b59a6ba99c0c369f87b2056b0f8ca63f518
Bas Kaal <3askaal@gmail.com>
2020-08-26 00:41:57 +0200

    fix github api authentication config

--- src/network/auth.tsx
+++ src/network/auth.tsx
@@ -1,12 +1,12 @@
 import { setContext } from 'apollo-link-context'
 
 export const authLink = setContext((_, { headers }) => {
-  const token: string | null = localStorage.getItem('token')
-
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${process.env.REACT_APP_PUBLIC_ACCESS_TOKEN}` : '',
+      authorization: process.env.REACT_APP_PUBLIC_ACCESS_TOKEN
+        ? `Bearer ${process.env.REACT_APP_PUBLIC_ACCESS_TOKEN}`
+        : '',
     },
   }
 })
